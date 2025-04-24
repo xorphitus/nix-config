1bb5da7d0e03c3f1c2f6b6247497b3d57c126ad8
xorphitus <xorphitus@gmail.com>
2025-04-24 14:15:27 +0900

    Update flake to support NixOS configuration
    - Added `flake.lock` for managing dependencies.  Updated `flake.nix`
    - to define a NixOS system configuration.

--- flake.nix
+++ flake.nix
@@ -1,15 +1,18 @@
 {
-  description = "A very basic flake";
+  description = "My NixOS flake config";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    nixosConfigurations = {
+      vm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/nixos/vm.nix
+        ];
+      };
+    };
   };
 }
