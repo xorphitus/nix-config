660419ddc7fd9b919e57f88a38a856c2eefcc552
xorphitus <xorphitus@gmail.com>
2025-04-25 16:26:29 +0900

    add gcc

--- hosts/nixos/desktop.nix
+++ hosts/nixos/desktop.nix
@@ -91,6 +91,7 @@ boot.loader = {
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     git
+    gcc
     emacs
     # GUI
     hyprland
