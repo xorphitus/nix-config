dc4181760cd86b151df11dff36aaf32667eaba2f
xorphitus <xorphitus@gmail.com>
2025-06-07 11:55:59 +0900

    add darwin key config

--- hosts/darwin/macbook-air-m2.nix
+++ hosts/darwin/macbook-air-m2.nix
@@ -24,6 +24,14 @@
 
   # https://nix-darwin.github.io/nix-darwin/manual/
 
+  system.keyboard.remapCapsLockToControl = true;
+
+  # Use F1, F2, etc. keys as standard function keys.
+  system.defaults.NSGlobalDomain."com.apple.keyboard.fnState" = true;
+
+  # Whether to enable trackpad tap to click. The default is false.
+  system.defaults.trackpad.Clicking = true;
+
   homebrew.enable = true;
 
   programs.fish.enable = true;
