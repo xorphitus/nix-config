84996928bb13bcdd7967061c955c03d18f364971
xorphitus <xorphitus@gmail.com>
2025-06-08 14:23:37 +0900

    darwin: emacs plus

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -35,4 +35,30 @@
     cmigemo
     brewCasks.musescore
   ];
+
+  programs.emacs = {
+    enable = true;
+    package = pkgs.emacs.overrideAttrs (old: {
+      patches =
+        (old.patches or [])
+        ++ [
+          # Fix OS window role (needed for window managers like yabai)
+          # NOTE:  emacs-30/fix-window-role.patch is  a link to  emacs-28/fix-window-role.patch
+          (pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/refs/heads/master/patches/emacs-28/fix-window-role.patch";
+            sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
+          })
+          # Enable rounded window with no decoration
+          (pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/refs/heads/master/patches/emacs-30/round-undecorated-frame.patch";
+            sha256 = "sha256-uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
+          })
+          # Make Emacs aware of OS-level light/dark mode
+          (pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/refs/heads/master/patches/emacs-30/system-appearance.patch";
+            sha256 = "sha256-3QLq91AQ6E921/W9nfDjdOUWR8YVsqBAT/W9c1woqAw=";
+          })
+        ];
+    });
+  };
 }
