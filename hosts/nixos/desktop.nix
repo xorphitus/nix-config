cfa07755d896df53137e894e362762119ec5cb58
xorphitus <xorphitus@gmail.com>
2025-10-10 19:09:08 +0900

    bluetooth (WIP)

--- hosts/nixos/desktop.nix
+++ hosts/nixos/desktop.nix
@@ -285,6 +285,31 @@
     "cdi/nvidia.yaml".source = ./desktop_nvidia.yaml;
   };
 
+  # Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+  };
+
+  services.blueman.enable = true;
+
   # Ollama
   services.ollama = {
     enable = true;
