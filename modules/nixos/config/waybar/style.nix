2ef19fc80e17156177727c9f893ca60a3ae6cf5b
xorphitus <xorphitus@gmail.com>
2026-04-22 22:38:03 +0900

    fix(waybar): Update styling to use rgba() for to resolve an error

--- modules/nixos/config/waybar/style.nix
+++ modules/nixos/config/waybar/style.nix
@@ -30,12 +30,12 @@
   #custom-media,
   #custom-swaync,
   #custom-pomodoro-el {
-      background: ${palette.nord0}cc;
+      background: rgba(${palette.nord0rgb}, 0.8);
       color: ${palette.nord6};
       padding: 0 10px;
       margin: 4px 2px;
       border-radius: 12px;
-      border: 1px solid ${palette.nord3}80;
+      border: 1px solid rgba(${palette.nord3rgb}, 0.5);
   }
 
   #workspaces button {
@@ -49,26 +49,26 @@
 
   #workspaces button.active {
       color: ${palette.nord6};
-      background: ${palette.nord8}66;
+      background: rgba(${palette.nord8rgb}, 0.4);
   }
 
   #workspaces button:hover {
-      background: ${palette.nord3}66;
+      background: rgba(${palette.nord3rgb}, 0.4);
       box-shadow: none;
       text-shadow: none;
   }
 
   #temperature.critical {
-      background: ${palette.nord11}cc;
+      background: rgba(${palette.nord11rgb}, 0.8);
   }
 
   #battery.warning {
-      background: ${palette.nord13}cc;
+      background: rgba(${palette.nord13rgb}, 0.8);
       color: ${palette.nord0};
   }
 
   #battery.critical {
-      background: ${palette.nord11}cc;
+      background: rgba(${palette.nord11rgb}, 0.8);
   }
 
   #idle_inhibitor.activated {
@@ -76,8 +76,8 @@
   }
 
   tooltip {
-      background: ${palette.nord1}f2;
-      border: 1px solid ${palette.nord3}80;
+      background: rgba(${palette.nord1rgb}, 0.95);
+      border: 1px solid rgba(${palette.nord3rgb}, 0.5);
       border-radius: 12px;
   }
 
--- modules/nixos/palette/nord.nix
+++ modules/nixos/palette/nord.nix
@@ -1,9 +1,15 @@
 {
   nord0 = "#2e3440";
+  nord0rgb = "46, 52, 64";
   nord1 = "#3b4252";
+  nord1rgb = "59, 66, 82";
   nord3 = "#4c566a";
+  nord3rgb = "76, 86, 106";
   nord6 = "#eceff4";
   nord8 = "#88c0d0";
+  nord8rgb = "136, 192, 208";
   nord11 = "#bf616a";
+  nord11rgb = "191, 97, 106";
   nord13 = "#ebcb8b";
+  nord13rgb = "235, 203, 139";
 }
